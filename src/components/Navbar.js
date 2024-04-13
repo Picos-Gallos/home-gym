7c0c52393ca19f285af6555d1151706229efab1b
Matt Sherrill <matthew@sherrillfamily.net>
2024-04-13 11:05:09 -0500

    added navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Navbar.css';
+// import GymIcon from '../img/icon.png';
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="logo">HomeGym</div>
+            <div className="logo">
+                HomeGym 
+                {/* <img src={GymIcon} alt="Gym Icon" style={{ width: '24px', height: '24px', marginLeft: '10px' }} /> */}
+            </div>
             <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/gyms">Gyms</a></li>
+                <li><a href="/table">Gyms</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/login" className="button">Sign In</a></li>
             </ul>
