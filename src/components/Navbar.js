d5227960adfd093f02a17c9a33855a0670746296
Matt Sherrill <matthew@sherrillfamily.net>
2024-04-13 13:53:41 -0500

    Fixing Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Navbar.css';
-// import GymIcon from '../img/icon.png';
+import GymIcon from '../img/icon.png';
 
 function Navbar() {
     return (
         <nav className="navbar">
             <div className="logo">
+                
+                <img src={GymIcon} alt="Gym Icon" style={{ width: '30px', height: '30px', marginLeft: '10px', marginRight: '10px' }} />
                 HomeGym 
-                {/* <img src={GymIcon} alt="Gym Icon" style={{ width: '24px', height: '24px', marginLeft: '10px' }} /> */}
             </div>
             <ul className="nav-links">
                 <li><a href="/table">Gyms</a></li>
