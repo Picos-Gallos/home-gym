06c67f0c8ca0c431ecc1e46f86d8c1f33062bc95
Cade DuPont <cadedupont45@gmail.com>
2024-04-12 23:58:57 -0500

    Connect login with Firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import logo from './logo.png';
-import './App.css';
+import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        var user = userCredential.user;
+        console.log('User registered: ', user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error: ', errorCode, errorMessage);
+      });
+  }
+
   return (
     <>
       <div className="login">
-        <h1>Login</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <input type={"submit"} style={{ backgroundColor: "#2c5237" }} />
+        <h1>Register</h1>
+        <form onSubmit={register}>
+          <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+          <input type="submit" style={{ backgroundColor: "#2c5237" }} />
         </form>
       </div>
     </>
