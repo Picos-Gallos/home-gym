156427691d8b63d240cf8787eabda2f5d3d0b22e
Cade DuPont <cadedupont45@gmail.com>
2024-04-12 23:39:50 -0500

    Firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="login">
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type={"submit"} style={{ backgroundColor: "#2c5237" }} />
+        </form>
+      </div>
+    </>
   );
 }
 
